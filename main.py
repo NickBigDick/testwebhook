ec23094e8da4a36cf29bc929762bd72689af8f9a
Nikolay <Kolya0715>
2023-02-26 11:08:27 +0300

    FIRST

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import telebot
+from telebot.types import Message
+from flask import Flask, request
+
+bot = telebot.TeleBot("5781580771:AAFe_HToFfz6QWgQVGJbj_YqTPCgwhmGPX8")
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}")
+
+@server.route(f"/", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook("https://92.255.67.180")
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
