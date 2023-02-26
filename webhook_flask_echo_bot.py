92c20ac08bc928a76dc960a9970f78a2c43bfd1f
Nikolay <Kolya0715>
2023-02-26 19:18:26 +0300

    FIRST

--- webhook_flask_echo_bot.py
+++ webhook_flask_echo_bot.py
@@ -5,12 +5,20 @@ import telebot
 
 token = "5881389387:AAFC2UbytWDG4fAGtXTISko9W3Qq0w0Gctw"
 
-WEBHOOK_HOST = "192.168.0.2"
+WEBHOOK_HOST = "92.255.67.180"
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = "192.168.0.2"  # In some VPS you may need to put here the IP addr
+WEBHOOK_LISTEN = "92.255.67.180"  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % token
