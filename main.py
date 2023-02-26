731666ab0012b1d04cef37cab6ff51dae8a78c0f
Nikolay <Kolya0715>
2023-02-26 11:28:57 +0300

    FIRST

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import telebot
 from telebot.types import Message
 from flask import Flask, request
+import logging
 
 token = "5781580771:AAFe_HToFfz6QWgQVGJbj_YqTPCgwhmGPX8"
 bot = telebot.TeleBot(token)
 server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
 
 @bot.message_handler(commands=['start'])
 def start(message: Message):
     username = message.from_user.username
     bot.reply_to(message, f"Hello, {username}")
+
 
 @server.route(f"/{token}", methods=["POST"])
 def redirect_message():
