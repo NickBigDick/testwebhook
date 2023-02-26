0c33b9a53561eb50917fd1a233359840dbc7b2fa
Nikolay <Kolya0715>
2023-02-26 22:15:20 +0300

    FIRST

--- webhook_flask_echo_bot.py
+++ webhook_flask_echo_bot.py
@@ -20,8 +20,8 @@ WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
 # with the same value in you put in WEBHOOK_HOST
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % token
+WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}:{WEBHOOK_PORT}"
+WEBHOOK_URL_PATH = f"/{token}/"
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
