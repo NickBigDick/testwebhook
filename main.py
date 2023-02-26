3a577df62ae6d2785d4c788ca75b867d3c588605
Nikolay <Kolya0715>
2023-02-26 12:37:45 +0300

    FIRST

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def redirect_message():
     json_string = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
+    print("mark print")
     return "!", 200
 
 
